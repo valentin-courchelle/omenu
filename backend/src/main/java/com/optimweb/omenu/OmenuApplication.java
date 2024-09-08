1956757ae2971a3e8273501ea7beff6136f9b9e1
valentin <valentin.courchelle@gmail.com>
2024-09-08 16:18:54 +0200

    Project initialization

---
+++ backend/src/main/java/com/optimweb/omenu/OmenuApplication.java
@@ -0,0 +1,13 @@
+package com.optimweb.omenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmenuApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/optimweb/omenu/OmenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimweb.omenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
