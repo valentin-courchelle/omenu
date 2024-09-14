fa27e7088e166889d6db0bf3a9fc59405c92dc70
valentin <valentin.courchelle@gmail.com>
2024-09-14 23:46:04 +0200

    create LoginController

---
+++ backend/src/main/java/com/optimweb/omenu/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.optimweb.omenu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/user")
+    public String getUser(){
+        return "Welcome User";
+    }
+
+    @GetMapping("/admin")
+    public String getAdmin(){
+        return "Welcome Admin";
+    }
+}
