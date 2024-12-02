bf0c9c233bb6b2a0e23da1da22444869deadb2e1
valentin <valentin.courchelle@gmail.com>
2024-12-03 00:22:01 +0100

    Add modification and deletion functionality on ingredient table

--- frontend/src/app/pages/recipes/ingredients-section/ingredients-section.component.ts
+++ frontend/src/app/pages/recipes/ingredients-section/ingredients-section.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { max } from 'rxjs';
 
 // Type pour les colonnes triables
 type IngredientColumn = 'name' | 'type' | 'recipeCount';
@@ -14,19 +15,19 @@ type IngredientColumn = 'name' | 'type' | 'recipeCount';
 })
 export class IngredientsSectionComponent {
   ingredients = [
-    { name: 'Carotte', type: 'Légume', recipeCount: 5 },
-    { name: 'Poulet', type: 'Viande', recipeCount: 10 },
-    { name: 'Saumon', type: 'Poisson', recipeCount: 3 },
-    { name: 'Emmental', type: 'Produit-laitier', recipeCount: 8 },
-    { name: 'Camembert', type: 'Produit-laitier', recipeCount: 1 },
-    { name: 'Courgettes', type: 'Légume', recipeCount: 2 },
-    { name: 'Tomates', type: 'Légume', recipeCount: 5 },
-    { name: 'Saumon', type: 'Poisson', recipeCount: 3 },
-    { name: 'Navet', type: 'Légume', recipeCount: 9 },
-    { name: 'Porc', type: 'Viande', recipeCount: 7 },
-    { name: 'Colin', type: 'Poisson', recipeCount: 2 },
-    { name: 'Brocoli', type: 'Légume', recipeCount: 1 },
-    { name: 'Crème fraiche', type: 'Produit-laitier', recipeCount: 8 }
+    { id: 1, name: 'Carotte', type: 'Légume', recipeCount: 5 },
+    { id: 2, name: 'Poulet', type: 'Viande', recipeCount: 10 },
+    { id: 3, name: 'Saumon', type: 'Poisson', recipeCount: 3 },
+    { id: 4, name: 'Emmental', type: 'Produit-laitier', recipeCount: 8 },
+    { id: 5, name: 'Camembert', type: 'Produit-laitier', recipeCount: 1 },
+    { id: 6, name: 'Courgettes', type: 'Légume', recipeCount: 2 },
+    { id: 7, name: 'Tomates', type: 'Légume', recipeCount: 5 },
+    { id: 8, name: 'Saumon', type: 'Poisson', recipeCount: 3 },
+    { id: 9, name: 'Navet', type: 'Légume', recipeCount: 9 },
+    { id: 10, name: 'Porc', type: 'Viande', recipeCount: 7 },
+    { id: 11, name: 'Colin', type: 'Poisson', recipeCount: 2 },
+    { id: 12, name: 'Brocoli', type: 'Légume', recipeCount: 0 },
+    { id: 13, name: 'Crème fraiche', type: 'Produit-laitier', recipeCount: 8 }
   ];
 
   ingredientTypes = ['Légume', 'Viande', 'Poisson', 'Produit-laitier'];
@@ -39,6 +40,15 @@ export class IngredientsSectionComponent {
   sortedIngredients = [...this.ingredients];
   sortColumn: IngredientColumn = 'name'; // Utilisation du type ici
   sortDirection: 'asc' | 'desc' | '' = '';
+
+  editedIngredientId: number | null = null;
+  editedField: string = '';
+
+  showPopup = false;
+  ingredientToDelete: {id: number, name:string, type:string, recipeCount: number} | undefined;
+
+  hoveredRow: number = -1;
+
 
   // Méthode pour trier par colonne
   sortBy(column: IngredientColumn) {
@@ -83,10 +93,18 @@ export class IngredientsSectionComponent {
     return '';
   }
 
+  generateId(): number {
+    return this.ingredients.length > 0
+      ? Math.max(...this.ingredients.map((ingredient) => ingredient.id)) + 1
+      : 1;
+  }
+
   // Ajouter un ingrédient
   addIngredient() {
     if (this.newIngredient.name.trim()) {
+      const maxId = this.ingredients.length > 0 ? Math.max(...this.ingredients.map(ingredient => ingredient.id)) : 1;
       this.ingredients.push({
+        id: this.generateId(),
         name: this.newIngredient.name.trim(),
         type: this.newIngredient.type,
         recipeCount: 0,
@@ -96,5 +114,41 @@ export class IngredientsSectionComponent {
     } else {
       alert('Veuillez entrer un nom pour l\'ingrédient.');
     }
+  }
+
+  editRow(id: number, field: string){
+    this.editedIngredientId = id;
+    this.editedField = field;
+  }
+
+  saveEdit(){
+    this.editedIngredientId = null;
+    this.editedField = '';
+    this.applySort();
+  }
+
+  deleteIngredient(id: number){
+    const ingredient = this.ingredients.find(i=>i.id === id);
+    if(!ingredient){
+      return;
+    }
+
+    this.ingredientToDelete = ingredient;
+    if(ingredient.recipeCount > 0){
+      this.showPopup = true;
+    } else{
+      this.confirmDelete();
+    }
+  }
+
+  confirmDelete(){
+    this.ingredients = this.ingredients.filter(i=>i.id !== this.ingredientToDelete?.id);
+    this.ingredientToDelete = undefined;
+    this.showPopup = false;
+    this.applySort();
+  }
+
+  cancelDelete(){
+    this.ingredientToDelete = undefined;
   }
 }
