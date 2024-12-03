3b2c1347fd52b34e773cbccec74fc17b700bde14
valentin <valentin.courchelle@gmail.com>
2024-12-03 21:26:16 +0100

    minor

--- frontend/src/app/pages/recipes/ingredients-section/type-color.ts
+++ frontend/src/app/pages/recipes/ingredients-section/type-color.ts
@@ -5,7 +5,7 @@ export const typeColors: Record<IngredientDto.TypeEnum, string> = {
     MEAT: '#D32F2F',
     MILK_PRODUCT: '#f5f5dc',
     SPICE: '#FF5722',
-    SALT_GROCERY: '#A1887F',
+    SALT_GROCERY: '#D7CCC8',
     SWEET_GROCERY: '#FFB6C1',
     FRUIT: '#FF9800',
     CONDIMENT: '#FFEB3B'
