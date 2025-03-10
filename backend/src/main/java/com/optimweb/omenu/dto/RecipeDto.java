48ccdf7af1ff98eaa2f1f8e675ac62a0e1f485e7
valentin <valentin.courchelle@gmail.com>
2025-03-10 23:30:46 +0100

    Add constraints on RecipeDto fields

--- backend/src/main/java/com/optimweb/omenu/dto/RecipeDto.java
+++ backend/src/main/java/com/optimweb/omenu/dto/RecipeDto.java
@@ -1,6 +1,8 @@
 package com.optimweb.omenu.dto;
 
 import com.optimweb.omenu.model.Month;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,17 +18,24 @@ public class RecipeDto {
 
     private Long id;
 
+    @NotNull
     private String name;
 
     private String description;
 
+    @NotNull
     private int duration;
 
+    @NotNull
     private float rating;
 
+    @NotNull
     private int nbPeople;
 
+    @NotNull
     private List<Month> season;
 
+    @NotNull
+    @NotEmpty
     private List<RecipeIngredientDto> ingredients;
 }
